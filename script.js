fcdd0b8f0d5fa7fcd5d68a1a74d4af5a7a520d9e
GabeValdivia <gabe@gabeswebcoding.com>
2022-02-01 20:54:36 -0600

    Refactor fuctions to use map

--- script.js
+++ script.js
@@ -16,20 +16,31 @@ async function searchSongs(term) {
 
 // Show song and artist in DOM
 function showData(data) {
-	let output = '';
+	// let output = '';
 
-	data.data.forEach(song => {
-		output += `
-			<li>
-				<span><strong>${song.artist.name}</strong> = ${song.title}</span>
-				<button class="btn" data-artist="${song.artist.name}" data-songtitle="${song.title}">Get Lyrics</button>
-			</li>
-		`;
-	});
+	// data.data.forEach(song => {
+	// 	output += `
+	// 		<li>
+	// 			<span><strong>${song.artist.name}</strong> = ${song.title}</span>
+	// 			<button class="btn" data-artist="${song.artist.name}" data-songtitle="${song.title}">Get Lyrics</button>
+	// 		</li>
+	// 	`;
+	// });
+
+	// result.innerHTML = `
+	// 	<ul class="songs">
+	// 		${output}
+	// 	</ul>
+	// `;
 
 	result.innerHTML = `
 		<ul class="songs">
-			${output}
+			${data.data.map(song => `
+				<li>
+					<span><strong>${song.artist.name}</strong> = ${song.title}</span>
+					<button class="btn" data-artist="${song.artist.name}" data-songtitle="${song.title}">Get Lyrics</button>
+				</li>
+			`).join('')}
 		</ul>
 	`;
 }
